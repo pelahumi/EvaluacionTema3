318c6771584330305b6aaa748a1d1fd18934032c
Pelayo <phuermij@myuax.com>
2022-10-28 16:13:48 +0200

    torres de hanoi

--- Ejercicio1/Ejercicio1.py
+++ Ejercicio1/Ejercicio1.py
@@ -17,12 +17,33 @@ for i in range(long):
     hanoi1.apilar(elementos[long - i -1])
 
 #Ahora creamos el algoritmo que resolvera el problema
-def torres_hanoi(torre1, torre2, torre3):
+def torres_hanoi(discos):
     torre1 = hanoi1
     torre2 = hanoi2
     torre3 = hanoi3
-    discos = 64
+
+    cima = torre1.en_cima()
+    cima2 = torre2.en_cima()
+    cima3 = torre3.en_cima()
     if discos == 1:
-        print(torre1.en_cima(), "va a ", torre3.
+        print(torre1.en_cima(), "va a la torre 3")
+    else:
+        if (cima < cima2) or (cima2 is None):
+            torre2.apilar(cima)
+            torre1.desapilar()
+        elif (cima < cima3) or (cima3 is None):
+            torre3.apilar(cima)
+            torre1.desapilar()
+        elif (cima2 < cima3) and (cima2 > cima):
+            torre3.apilar(cima2)
+            torre2.desapilar()
+        elif (cima2 < cima) and (cima2 > cima3):
+            torre1.apilar(cima2)
+            torre2.desapilar()
+        elif (cima3 < cima
+
+
+        
+        
     
 
