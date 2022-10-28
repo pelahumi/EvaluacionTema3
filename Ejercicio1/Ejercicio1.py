f4071f4a8c4d34d4a45a51f836e6e3c08f217ca5
Pelayo <phuermij@myuax.com>
2022-10-28 15:46:24 +0200

    init

---
+++ Ejercicio1/Ejercicio1.py
@@ -0,0 +1,13 @@
+from Ejercicio1.Pila import Pila
+
+#Creamos las tres torres de hanoi como objetos pila
+
+hanoi1 = Pila()
+hanoi2 = Pila()
+hanoi3 = Pila()
+
+#Creamos una lista con 64 elementos 
+elementos = []
+for i in range (64):
+    elementos.append(i)
+print(elementos)
