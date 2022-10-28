d960122ae11b40deb0d7bc7c2a795bbfa6c35bc0
Pelayo <phuermij@myuax.com>
2022-10-28 16:02:44 +0200

    cambios

--- Ejercicio1/Ejercicio1.py
+++ Ejercicio1/Ejercicio1.py
@@ -1,4 +1,4 @@
-from Ejercicio1.Pila import Pila
+from Pila import Pila
 
 #Creamos las tres torres de hanoi como objetos pila
 
@@ -8,6 +8,21 @@ hanoi3 = Pila()
 
 #Creamos una lista con 64 elementos 
 elementos = []
-for i in range (64):
+for i in range (1,65):
     elementos.append(i)
-print(elementos)
+
+#Los apilamos en la primera torre
+long = len(elementos)
+for i in range(long):
+    hanoi1.apilar(elementos[long - i -1])
+
+#Ahora creamos el algoritmo que resolvera el problema
+def torres_hanoi(torre1, torre2, torre3):
+    torre1 = hanoi1
+    torre2 = hanoi2
+    torre3 = hanoi3
+    discos = 64
+    if discos == 1:
+        print(torre1.en_cima(), "va a ", torre3.
+    
+
