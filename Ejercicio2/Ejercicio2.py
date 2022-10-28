d4721641d3d9e5a4e369141951f64e9ac5666288
Pelayo <phuermij@myuax.com>
2022-10-28 16:36:21 +0200

    determinante iterativo

--- Ejercicio2/Ejercicio2.py
+++ Ejercicio2/Ejercicio2.py
@@ -21,11 +21,16 @@ def determinante(matriz):
     for i in range(len(matriz)):
         dp2 = dp2 + matriz[i][j]
     
-    
+    s1 = matriz[0][1] * matriz[1][2] * matriz[2][0]
+    s2 = matriz[1][0] * matriz[2][1] * matriz[0][2]
 
+    r1= matriz[0][1] * matriz[1][0] * matriz[2][2]
+    r2 = matriz[2][1] * matriz[1][2] * matriz[0][0]
+ 
+    detereminante = dp + s1 + s2 - (dp2 + r1 + r2)
     
+    return determinante
 
-    
             
 
 
