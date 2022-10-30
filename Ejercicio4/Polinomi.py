d0643bdd6808087f6beb8ec0c5c4a60236036a62
Pelayo <phuermij@myuax.com>
2022-10-30 13:30:15 +0100

    clase dato polinomio

--- Ejercicio4/Polinomi.py
+++ Ejercicio4/Polinomi.py
@@ -1,8 +1,10 @@
 #Creamos la clase nodo
-from distutils.log import info
-
-
 class Nodo():
     def __init__(self, info=None, sig=None):
         self.info = info
         self.sig = sig
+
+class DatoPolinomio():
+    def __init__(self, valor, termino):
+        self.valor = valor
+        self.termino = termino
