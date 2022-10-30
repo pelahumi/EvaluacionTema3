1797cc7deb7c0072b4b8426f433d92e95d359fbb
Pelayo <phuermij@myuax.com>
2022-10-30 13:32:40 +0100

    Explicacion del codigo

--- Ejercicio4/Polinomi.py
+++ Ejercicio4/Polinomi.py
@@ -4,11 +4,13 @@ class Nodo():
         self.info = info
         self.sig = sig
 
+#Creamos la clase dato polinomio que crea un polinomio con valor y término
 class DatoPolinomio():
     def __init__(self, valor, termino):
         self.valor = valor
         self.termino = termino
 
+#Creamos la clase polinomio que crea un polinomio de grado 0
 class Polinomio():
     def __init__(self):
         self.termino_mayor = None
