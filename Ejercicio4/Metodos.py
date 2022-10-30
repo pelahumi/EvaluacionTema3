24f61fbaacf82365a4561fc57a68f7a40d149213
Pelayo <phuermij@myuax.com>
2022-10-30 13:45:13 +0100

    funcion obtener valor

--- Ejercicio4/Metodos.py
+++ Ejercicio4/Metodos.py
@@ -26,4 +26,11 @@ def modificar_termino(polinomio, termino, valor):
         aux = aux.sig
         aux.info.valor = valor
     
-    
+def obtener_valor(polinomio, termino):
+    aux = polinomio.termino_mayor
+    while (aux is not None) and (aux.info.termino > termino):
+        aux = aux.sig
+    if (aux is not None) and (aux.info.termino == termino):
+        return aux.info.valor
+    else:
+        return 0
