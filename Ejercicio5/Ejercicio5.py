7a8ed28640e01e3e159c1193ba1ac1d8882a2cb8
Pelayo <phuermij@myuax.com>
2022-10-30 19:40:21 +0100

    cifrado y descifrado

--- Ejercicio5/Ejercicio5.py
+++ Ejercicio5/Ejercicio5.py
@@ -1,27 +1,31 @@
 #Importamos la libreria hashlib
 import hashlib
-from hmac import digest_size
+
 
 class Cifrado():
     def __init__(self, mensaje):
         self.mesaje = str(input("Introduce el mensaje que quieres enviar: ").encode("utf-8"))
-        self.cifrado = None
 
     def cifrar(self):
         code = hashlib.blake2s()
         code.update(self.mesaje)
-        self.cifrado = code.hexdigest()
-        return self.cifrado
+        cifrado = code.hexdigest()
+        return cifrado
     
     def descifrar(self):
         encode = str(input("Introduce el mensaje a deescifrar: "))
-        descifrador = open("palabras.txt", "r")
+        code = hashlib.blake2s()
+        code.update(self.mensaje)
 
-        for x in descifrador.readlines():
-            a = x.strip("\n").encode("utf-8")
-            b = hashlib.blake2s(a).hexdigest()
+        if encode == code:
+            print("El mensaje es: ",self.mesaje)
+        else:
+            print("El mensaje cifrado no coincide con el mensaje enviado")
 
-        
-        if a == encode:
+
+
+
+
+ 
             
 
