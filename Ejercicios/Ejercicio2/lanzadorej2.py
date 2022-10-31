caa02efb2a6962abf630f9301f8a0e2248681f8d
Pelayo <phuermij@myuax.com>
2022-10-31 16:34:11 +0100

    lanzador ejercicio 2

---
+++ Ejercicios/Ejercicio2/lanzadorej2.py
@@ -0,0 +1,7 @@
+from ejercicio2 import matriz, determinante
+
+def ejercicio2():
+    print("Creamos la matriz:\n")
+    print(matriz)
+    print("\n")
+    print("Este es el determinante: {}".format(determinante()))
